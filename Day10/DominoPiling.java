9f62ab1d593db041f2e0d027376e1b9aacdddb87
Devtyagi7 <devt3650@gmail.com>
2026-06-15 21:25:06 +0530

    Add Day10 LeetCode and Codeforces solutions

---
+++ Day10/DominoPiling.java
@@ -0,0 +1,13 @@
+package Day10;
+import java.util.*;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println((m * n) / 2);
+    }
+}
