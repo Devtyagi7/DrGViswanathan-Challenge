3e8009eb9d489b9ea74042e0d71a2f4d318dd597
Devtyagi7 <devt3650@gmail.com>
2026-06-25 16:53:22 +0530

    Day 20 - Number of Steps to Reduce a Number to Zero

---
+++ Day20/NumberOfSteps.java
@@ -0,0 +1,28 @@
+package Day20;
+public class NumberOfSteps {
+
+    public static int numberOfSteps(int num) {
+
+        int steps = 0;
+
+        while (num != 0) {
+
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+
+    public static void main(String[] args) {
+
+        int num = 14;
+
+        System.out.println("Number of Steps: " + numberOfSteps(num));
+    }
+}
